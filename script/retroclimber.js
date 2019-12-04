ddf5ad27028c41bb080752b75ea4df5074ed603f
unknown <selvaggio.adam@gmail.com>
2019-12-04 16:42:36 +0100

    Init movement with player

--- script/retroclimber.js
+++ script/retroclimber.js
@@ -1,11 +1,11 @@
 var config = {
     type: Phaser.AUTO,
     width: 720,
-    height: 800,
+    height: 1280,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 400 },
             debug: false
         }
     },
@@ -24,24 +24,42 @@ var cursors;
 
 function preload () {
     this.load.image('sky', 'img/sky3.jpg');
-    this.load.image('ground', 'img/tile2.png');
+    this.load.image('ground', 'img/tile1.png');
+    this.load.image('groundf', 'img/groundf.png');
     this.load.spritesheet('hero', 'img/herof.png', { frameWidth: 55, frameHeight: 95 });
 }
 
 
 function create () {
+    //this.cameras.main.setBounds(0, 0, 720, 1280).setName('main');
     this.add.image(0, 0, 'sky').setOrigin(0, 0);
 
     platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground');
-    platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+
+    platforms.create(400, 540, 'ground');
+    platforms.create(450, 540, 'ground');
+
+    platforms.create(150, 720, 'ground');
+    platforms.create(200, 720, 'ground');
+
+    platforms.create(250, 910, 'ground');
+    platforms.create(300, 910, 'ground');
+
+    platforms.create(450, 1090, 'ground');
+    platforms.create(500, 1090, 'ground');
+
+    platforms.create(350, 1280, 'groundf');
+
+    // platforms.create(800, 600, 'ground');
+    // platforms.create(750, 220, 'ground');
 
     player = this.physics.add.sprite(100, 450, 'hero');
+    player.y=0;
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
     this.physics.add.collider(player, platforms);
+    espace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
     this.anims.create({
         key: 'left',
@@ -80,6 +98,9 @@ function update () {
         player.anims.play('turn');
     }
     if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
+        console.log(this);
+        player.setVelocityY(-400);
     }
+
+    this.cameras.main.scrollY = player.y -400;
 }
